16806e398de254dfd5f6c444be2c76575aad49d9
Jordan <jcr.salazar@outlook.com>
2020-01-29 18:07:30 -0500

    ...

--- src/main/java/com/rafalazar/bootcamp/app/document/Client.java
+++ src/main/java/com/rafalazar/bootcamp/app/document/Client.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
@@ -13,13 +15,12 @@ public class Client {
 	
 	@Id
 	private String id;
-	private String name;
-	private String lasName;
-	private String dni;
-	private String ruc;
-	private String socialName;
+	private String fullName;
+	private String numDoc;
 	private String address;
+	private String bank;
 	private String type;
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date joinAt;
 
 }
--- src/main/java/com/rafalazar/bootcamp/app/impl/ClientServiceImpl.java
+++ src/main/java/com/rafalazar/bootcamp/app/impl/ClientServiceImpl.java
@@ -13,7 +13,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
-public class ClientServiceImpl implements ClientService{
+public class ClientServiceImpl implements ClientService {
 
 	@Autowired
 	private ClientRepository repo;
@@ -30,36 +30,67 @@ public class ClientServiceImpl implements ClientService{
 
 	@Override
 	public Mono<Client> save(Client cl) {
-		if(cl.getJoinAt() == null) {
+		if (cl.getJoinAt() == null) {
 			cl.setJoinAt(new Date());
+		}else {
+			cl.setJoinAt(cl.getJoinAt());
 		}
+		
 		return repo.save(cl);
 	}
 
+	//Esta es la manera correcta de actualizar
 	@Override
 	public Mono<Client> update(Client cl, String id) {
 		return repo.findById(id)
 				.flatMap(c -> {
+					
+					//Fecha joinAt
 					if(cl.getJoinAt() == null) {
 						c.setJoinAt(new Date());
+					}else {
+						c.setJoinAt(cl.getJoinAt());
 					}
 					
-					if(c.getType() == "Personal") {
-						c.setName(cl.getName());
-						c.setLasName(cl.getLasName());
-						c.setDni(cl.getDni());
-						c.setAddress(cl.getAddress());
-						c.setType(cl.getType());
+					//FullName
+					if(cl.getFullName() == null) {
+						c.setFullName(c.getFullName());
 					}else {
-						c.setRuc(cl.getRuc());
-						c.setSocialName(cl.getSocialName());
+						c.setFullName(cl.getFullName());
+					}
+					
+					//NumDoc
+					if(cl.getNumDoc() == null) {
+						c.setNumDoc(c.getNumDoc());
+					}else {
+						c.setNumDoc(cl.getNumDoc());
+					}
+					
+					//Address
+					if(cl.getAddress() == null) {
+						c.setAddress(c.getAddress());
+					}else {
 						c.setAddress(cl.getAddress());
+					}
+					
+					//Bank
+					if(cl.getBank() == null) {
+						c.setBank(c.getBank());
+					}else {
+						c.setBank(cl.getBank());
+					}
+					
+					//Type
+					if(cl.getType() == null) {
+						c.setType(c.getType());
+					}else {
 						c.setType(cl.getType());
 					}
 					
 					return repo.save(c);
 					
 				});
+
 	}
 
 	@Override
