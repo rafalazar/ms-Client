0e9e97b81a324b161240da3683a6ccacc6b0a004
Jordan <jcr.salazar@outlook.com>
2020-01-30 11:41:58 -0500

    @Query agregado para validar el banco

--- src/main/java/com/rafalazar/bootcamp/app/repository/ClientRepository.java
+++ src/main/java/com/rafalazar/bootcamp/app/repository/ClientRepository.java
@@ -1,10 +1,14 @@
 package com.rafalazar.bootcamp.app.repository;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.rafalazar.bootcamp.app.document.Client;
 
+import reactor.core.publisher.Mono;
+
 
 public interface ClientRepository extends ReactiveMongoRepository<Client, String>{
-
+	@Query("{'bank' : ?0}")
+	public Mono<Client> updateBank(String bank, String id);
 }
